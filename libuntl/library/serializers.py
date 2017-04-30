8507e9a77b6dc8e348bf568065a4ba9cd570c7cd
joshbrooks <josh.vdbroek@gmail.com>
2017-04-30 12:38:42 +0900

    Library files

--- libuntl/library/serializers.py
+++ libuntl/library/serializers.py
@@ -1,8 +1,24 @@
+from rest_framework.filters import BaseFilterBackend
+
 from . import models
 from rest_framework import serializers, viewsets
 from rest_framework import routers
+import django_filters
 
 router = routers.SimpleRouter()
+
+
+class TimeStampFilter(BaseFilterBackend):
+    """
+    Filter that returns only objects modified since a given date
+    """
+    def filter_queryset(self, request, queryset, view):
+        if 'modified__gt' in request.GET:
+            queryset = queryset.filter(modified__gt=request.GET['modified__gt'])
+        if 'modified__lt' in request.GET:
+            queryset = queryset.filter(modified__gt=request.GET['modified__gt'])
+        return queryset
+
 
 class OrganizationModelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,6 +29,8 @@ class OrganizationModelSerializer(serializers.ModelSerializer):
 class OrganizationViewSet(viewsets.ModelViewSet):
     queryset = models.Organization.objects.all()
     serializer_class = OrganizationModelSerializer
+    filter_backends=(TimeStampFilter,)
+    ordering_fields='modified'
 
 
 router.register(r'organization', OrganizationViewSet)
@@ -70,7 +88,5 @@ class TagViewSet(viewsets.ModelViewSet):
     queryset = models.Tag.objects.all()
     serializer_class = TagModelSerializer
 
-
 router.register(r'tag', TagViewSet)
-
 urlpatterns = router.urls
