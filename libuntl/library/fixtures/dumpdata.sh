bd2f3d87dfbe209824b294225e3649093be48193
joshbrooks <josh.vdbroek@gmail.com>
2017-05-02 15:46:15 +0900

    offset modified timestamps for uniqueness

---
+++ libuntl/library/fixtures/dumpdata.sh
@@ -0,0 +1,6 @@
+python ../../manage.py dumpdata --indent=2 library.resource > ./resource.json
+python ../../manage.py dumpdata --indent=2 library.link > ./link.json
+python ../../manage.py dumpdata --indent=2 library.organization > ./organization.json
+python ../../manage.py dumpdata --indent=2 library.orgtype > ./orgtype.json
+python ../../manage.py dumpdata --indent=2 library.pubtype > ./pubtype.json
+python ../../manage.py dumpdata --indent=2 library.tag > ./tag.json
