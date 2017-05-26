7dd7a9f40ed56795968cb8b2c8426f4dffbd5f0d
joshbrooks <josh.vdbroek@gmail.com>
2017-05-26 14:15:20 +0900

    Work in progress on StorePagination

--- libuntl/static/riot/library/resource.store.js
+++ libuntl/static/riot/library/resource.store.js
@@ -3,38 +3,18 @@
  */
 
 window.stores = window.stores || {};
-(function(stores, resources){
-    var store_name = 'resource';
+(function (stores, resources) {
     var store;
+    var store_name = 'resource';
 
     /**
      * Initialize the database
      * @param database_opts
      * @returns {Promise<DB>}
      */
-    function initialiseIdb(database_opts){
-        /**
-         * options include name for the database, name for the table, version, keyPath, indexes
-         * @type {{dbname: string, version: number, objectStoreName: string, keyPath: string, indexes: [*]}}
-         */
-        // TODO: Input validation for this function
-        var opts = _.extend({}, database_opts);
 
-        return idb.open(opts.dbname, opts.version, function(upgradeDb){
-            switch(upgradeDb.oldVersion){
-                /* Add additional schema upgrades here */
-                case 0:
-                    var idbstore = upgradeDb.createObjectStore(opts.objectStoreName, {keyPath: opts.keyPath});
-                    _.each(indexes, function initIndex(i){
-                        idbstore.createIndex(i.indexName, i.field)
-                    });
-            }
-        })
-    }
-
-    function ResourceStore(resources, store_opts) {
+    function ResourceStore(store_opts) {
         var store = this;
-
         var defaults = {
             functionprefix: 'resources',
             dbname: 'libuntl',
@@ -55,19 +35,33 @@ window.stores = window.stores || {};
 
         riot.observable(this);
         store.urls = {
-            list: function(){return Urls.resource_list()},
-            detail: function(detail_id){return Urls.resource_detail(detail_id)}
+            list: function () {
+                return Urls.resource_list();
+            },
+            detail: function (detail_id) {
+                return Urls.resource_detail(detail_id);
+            }
         };
 
-        // store.on('refresh', function(last_modified){updateModifiedSince(last_modified)});
+        store.initialiseIdb(store.opts);
+        store.on('refresh', function (last_modified) {
+            store.update(last_modified);
+        });
+
+        store.on('update-start',function(opts){console.log('update-start', store, opts);})
+        store.on('update-continued',function(opts){store.getAll()})
+        store.on('update-end',function(opts){store.getAll()})
+        store.on('refresh',function(opts){console.log('refresh', store, opts);})
+
         // store.refresh();
     }
 
-    _.extend( ResourceStore.prototype, StoreMixin );
-    _.extend( ResourceStore.prototype, StoreIDBMixin );
+    _.extend(ResourceStore.prototype, mixins.StoreMixin);
+    _.extend(ResourceStore.prototype, mixins.StoreIDBMixin);
 
     stores[store_name] = new ResourceStore(resources);
-
     store = stores[store_name];
+    store.initialiseIdb(store.opts);
+    store.get_last_modified();
 
 }(window.stores, []));
